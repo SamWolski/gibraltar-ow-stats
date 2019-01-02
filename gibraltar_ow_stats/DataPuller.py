78b8a2803805789b9d8ed85f102c337588906c13
SamWolski <wlssam007@myuct.ac.za>
2019-01-02 21:12:39 +0900

    Move account parameters to DataPuller attributes
    These will now be required on object initialization. Hopefully this will allow for an easier way to maintain multiple DataPuller instances for different accounts within one script, as the idea is to tie pulling the data to saving the data.

--- gibraltar_ow_stats/DataPuller.py
+++ gibraltar_ow_stats/DataPuller.py
@@ -13,21 +13,26 @@ logger = logging.getLogger('gibraltar_ow_stats.DataPuller')
 
 class DataPuller:
 
-	def __init__(self):
+	def __init__(self, battletag, battletag_ID, region):
 		self.logger = logging.getLogger('gibraltar_ow_stats.DataPuller.DataPuller')
 		self.logger.debug('Initializing DataPuller instance...')
+		## Set attributes
+		self.battletag = battletag
+		self.battletag_ID = battletag_ID
+		self.region = region
+		##
 		self.logger.debug('DataPuller instance initialized.')
 
-	def pull_data(self, battletag, battletag_ID, region):
+	def pull_data(self):
 		'''
 		Get data for the given account
 
 		Uses the web API specified in the config file. Currently PC only.
 		'''
 		## Convert battletag and numeric ID to full username
-		username = battletag+'-'+str(battletag_ID)
+		username = self.battletag+'-'+str(self.battletag_ID)
 		## Generate target url based on config
-		target_url = config.get('api', 'url').format(region=region, username=username)
+		target_url = config.get('api', 'url').format(region=self.region, username=username)
 		self.logger.info('Fetching data from {}'.format(target_url))
 		## Fetch data
 		self.fetched_data = json.loads(urllib.request.urlopen(target_url).read())
