68a96da31df1f20399b032fe0280d7050bd50568
SamWolski <wlssam007@myuct.ac.za>
2019-01-02 11:14:18 +0900

    Add init script

---
+++ gibraltar_ow_stats/__init__.py
@@ -0,0 +1,10 @@
+'''
+Gibraltar: Overwatch stats tracking and analysis
+==================================================
+
+Gibraltar (named after Winston's office location) pulls your Overwatch stats
+from any web-based API, stores them as JSON files, and provides useful
+functions for analysis and seeing trends over time.
+'''
+
+__version__ = '0.1.dev0'
