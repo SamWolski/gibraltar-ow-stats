b2a9b5a66c0920a603e130c5f966d64d37b87d0b
SamWolski <wlssam007@myuct.ac.za>
2019-01-02 15:58:58 +0900

    Move config loading to _config script
    This allows all the modules in the package to share the same config, which is desired.

--- gibraltar_ow_stats/__init__.py
+++ gibraltar_ow_stats/__init__.py
@@ -12,29 +12,10 @@ import logging
 import logging.config
 
 from _logging_conf import LOGGING_CONF
-import _config
 
 logging.config.dictConfig(LOGGING_CONF)
 logger = logging.getLogger(__name__)
 
-__version__ = '0.1.dev2'
+from ._config import config
 
-## Config file
-CONFIG_PATH = os.path.expanduser('~/.config/gibraltar-ow-stats/gibraltar.ini')
-logger.info('Looking for config file at {}'.format(CONFIG_PATH))
-if not os.path.exists(CONFIG_PATH):
-	logger.info('Config file does not exist; auto-generating new config...')
-	new_config = _config.generate_new_config()
-	new_config_path = _config.write_config(new_config, CONFIG_PATH)
-	logger.info('New config file generated at {}'.format(new_config_path))
-## Load from file
-logger.info('Reading config file at {}'.format(CONFIG_PATH))
-config = _config.get_config(CONFIG_PATH)
-logger.info('Config loaded.')
-## Preview values
-logger.debug('Current config values:')
-for section in config.sections():
-	logger.debug('Section: {}'.format(section))
-	for options in config.options(section):
-		logger.debug('{} {} {}'.format(options, config.get(section, options), str(type(options))))
-##
+__version__ = '0.1.dev3'
--- gibraltar_ow_stats/_config.py
+++ gibraltar_ow_stats/_config.py
@@ -2,19 +2,29 @@
 Config parameters for Gibraltar.
 '''
 
+import os
 import ConfigParser
 import io
+import logging
+
+logger = logging.getLogger('gibraltar_ow_stats._config')
 
 DEFAULT_CONFIG_DICT = {
+	'localfiles': {
+		'working_dir': '~/gibraltar-ow-stats/',
+	},
 	'userprofile': {
 		'battletag': 'Surefour',
 		'battletag_id': 2559,
+		'platform': 'pc',
+		'region': 'us',
 	},
 	'api': {
 		'url': 'https://ovrstat.com/stats/pc/{region}/{username}',
 	},
 }
 
+DEFAULT_CONFIG_PATH = os.path.expanduser('~/.config/gibraltar-ow-stats/gibraltar.ini')
 
 def generate_new_config(config_dict=DEFAULT_CONFIG_DICT):
 	new_config = ConfigParser.ConfigParser()
@@ -43,3 +53,22 @@ def get_config(config_path):
 	config_data = read_config(config_path)
 	config = parse_config(config_data)
 	return config
+
+## Load config file
+logger.info('Looking for config file at {}'.format(DEFAULT_CONFIG_PATH))
+if not os.path.exists(DEFAULT_CONFIG_PATH):
+	logger.info('Config file does not exist; auto-generating new config...')
+	new_config = generate_new_config()
+	new_config_path = write_config(new_config, DEFAULT_CONFIG_PATH)
+	logger.info('New config file generated at {}'.format(new_config_path))
+## Load from file
+logger.info('Reading config file at {}'.format(DEFAULT_CONFIG_PATH))
+config = get_config(DEFAULT_CONFIG_PATH)
+logger.info('Config loaded.')
+## Preview values
+logger.debug('Current config values:')
+for section in config.sections():
+	logger.debug('Section: {}'.format(section))
+	for options in config.options(section):
+		logger.debug('{} {} {}'.format(options, config.get(section, options), str(type(options))))
+##
